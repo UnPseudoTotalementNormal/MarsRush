611884b5b74f99330313dbe733f98e7ff73293db
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2024-03-20 11:36:21 +0100

    modified game to upload

--- Script/Engine.gd
+++ Script/Engine.gd
@@ -3,4 +3,4 @@ var health_points = 10
 
 func _process(delta):
 	if health_points <= 0:
-		get_tree().change_scene_to_file("res://Levels/endscreen.tscn")
+		get_tree().change_scene_to_file("res://Levels/menu.tscn")
